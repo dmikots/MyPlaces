2013085d988dc86b89d5e305b7fe0bba00f62ea5
dmikots <57399131+dmikots@users.noreply.github.com>
2020-12-04 21:06:41 +0300

    add Custom View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Р71 on 04.12.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageOfPlace: UIImageView!
+}
