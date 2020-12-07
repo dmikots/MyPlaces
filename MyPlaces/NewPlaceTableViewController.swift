835ba7ed80a98d43fe36b44bdd360c1bca712af9
dmikots <57399131+dmikots@users.noreply.github.com>
2020-12-07 19:22:50 +0300

    add camera and libery. TODO logic

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -19,7 +19,21 @@ class NewPlaceTableViewController: UITableViewController {
         // если тапнули на 1 ячейку( выбор картинки)
         if indexPath.row == 0
         {
-            
+                let actionSheet = UIAlertController(title: nil,
+                                                    message: nil,
+                                                    preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(sourse: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(sourse: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            // вызов контроллера
+            present(actionSheet, animated: true)
         }
         // иначе скрываем клавиатуру
         else{
@@ -28,7 +42,7 @@ class NewPlaceTableViewController: UITableViewController {
     }
    
 }
-// MARK Text Field Dilegete
+//MARK: Text Field Dilegete
 extension NewPlaceTableViewController: UITextFieldDelegate{
     // скрываем клавиатуру по нажатию Done
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -36,4 +50,17 @@ extension NewPlaceTableViewController: UITextFieldDelegate{
         return true
     }
 }
-
+//MARK: Work with Image
+extension NewPlaceTableViewController{
+    func chooseImagePicker(sourse: UIImagePickerController.SourceType)
+    {
+        if UIImagePickerController.isSourceTypeAvailable(sourse)
+        {
+            let imagePicker = UIImagePickerController()
+            // Позволяет пользователю редактировать изображения
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = sourse
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+}
